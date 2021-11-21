3036f6d579449e8764fb14824273ed690c63c815
YATENDRA AWANA <yka6739@gmail.com>
2021-11-21 19:19:35 +0530

    Add project files.

---
+++ Start/Shared/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start.Shared.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string  Biography { get; set; }
+        public string  Picture { get; set; }
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
